40142178eca913bc35c5da69c35e91011492694a
vrishabh46 <157875661+vrishabh46@users.noreply.github.com>
2025-08-13 16:20:28 +0000

    done with further arrays

---
+++ 2.2Arrays.js
@@ -0,0 +1,16 @@
+const marvel = ["ironman" , "thor", "captain"]
+const dc = ["superman", "batman", "flash"]
+// marvel.push(dc) // array within array
+// console.log(marvel)
+// console.log(marvel[3],[1]);  // here if we want to print a substring index then we have to define substring index after main index to get the exact location.
+
+// const heros = marvel.concat(dc) // the concat function properly joins 2 strings so for that we have to definr the third variable which stores the concated string.
+// console.log(heros)
+
+// using spread fnc
+// const allheros = [...marvel, ...dc]  //can add more than 2 strings
+// console.log(allheros)  
+
+// some more to add  in this file 
+
+
